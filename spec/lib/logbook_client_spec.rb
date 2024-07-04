c018272f68082911d6ca7a97e4d9c6890d8cc938
Mauricio Lima <mauricio.prl42@gmail.com>
2024-07-04 17:57:38 -0300

    Add Document Helper (#2)
    * Add Document Helper
    
    * cops
    
    * lint
    
    * add missing specs
    
    * lint

---
+++ spec/lib/logbook_client_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+RSpec.describe LogbookClient do
+  describe 'VERSION' do
+    it { expect(described_class::VERSION).to eq('0.3.0') }
+  end
+
+  describe '#to_reference_id' do
+    subject(:to_reference_id) { described_class.to_reference_id(params) }
+
+    let(:params) { { integration_id: 1, log_id: 2, id: 3 } }
+
+    it { is_expected.to eq('integration_id:1--$/#--log_id:2--$/#--id:3') }
+
+    context 'when params is not valid' do
+      let(:params) { 'invalid' }
+
+      it 'raises the expected error' do
+        expect { to_reference_id }.to raise_error(LogbookClient::Error,
+                                                  'This method only accepts a Hash to be parsed')
+      end
+    end
+  end
+
+  describe '#to_searchable_terms' do
+    subject(:to_searchable_terms) { described_class.to_searchable_terms(param) }
+
+    context 'when param is a hash' do
+      let(:param) { { integration_id: 1, log_id: 2, id: 3 } }
+
+      it { is_expected.to eq(%w[integration_id:1 log_id:2 id:3]) }
+    end
+
+    context 'when the param is some document reference_id' do
+      let(:param) { 'integration_id:1--$/#--log_id:2--$/#--id:3' }
+
+      it { is_expected.to eq(%w[integration_id:1 log_id:2 id:3]) }
+    end
+  end
+end
